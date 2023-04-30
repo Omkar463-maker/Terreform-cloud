27c2f25086003d7f19f2409ce95fa3eada42617a
Omkar463-maker <124853573+Omkar463-maker@users.noreply.github.com>
2023-04-29 21:55:33 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-variable "aws_access_key_id" {}
-variable "aws_secret_key_id" {}
+variable "access_key" {}
+variable "secret_key" {}
 resource "aws_instance" "web" {
   ami           = "ami-a1b2c3d4"
   instance_type = "t2.micro"
