b00656a566b56f8016af93e717c550096c86b505
Omkar463-maker <124853573+Omkar463-maker@users.noreply.github.com>
2023-04-29 21:52:41 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-1"
-  access_key= "var.access_key"
-  secret_key="var.secret_key"
+  aws_access_key_id = "var.aws_access_key_id"
+  aws_secret_key_id = "var.aws_secret_key_id"
 }
