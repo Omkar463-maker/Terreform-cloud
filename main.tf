683017e89a80eaad2b0afed3e14355ac0fffad1e
Omkar463-maker <124853573+Omkar463-maker@users.noreply.github.com>
2023-04-29 19:59:54 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-  access_key= "var.access_key"
-  secret_key="var.Secret_key"
-}
 resource "aws_instance" "web" {
   ami           = "ami-a1b2c3d4"
   instance_type = "t2.micro"
